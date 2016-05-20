ef8e1fbcd77df7111f18695de5fc82ebb0a588f7
ledasko <dasko93@gmail.com>
2016-05-20 04:05:23 +0200

    v0.6
    Working main menu, death menu, pause menu. Fixed highscore. Added
    destroy to ALL pooled objects (fruit, dynamite, bombs, cacti, ground).
    Refoldered scenes and scripts for consistence.
    
    Problems:
    - ground randomly stops or acts as if something is there (invisibile)
    - cacti don't instakill even though they should (problem with
    boxCollider?)
    - ^no death sequence means the death menu cannot be tested

---
+++ AnimalSurvival/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    private PlayerController playerController;
+    private ScoreManager scoreManager;
+    public int scoreValue;
+    public float fruitValue;
+    public GameObject DestructionPoint;
+
+    void Start()
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+        GameObject playerControllerObject = GameObject.FindWithTag("Player");
+
+        if (playerControllerObject != null)
+        {
+            playerController = playerControllerObject.GetComponent<PlayerController>();
+        }
+
+        if(playerControllerObject == null)
+        {
+            Debug.Log("error");
+        }
+        DestructionPoint = GameObject.Find("DestructionPoint");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerHealth playerHealth = playerController.GetComponent<PlayerHealth>();
+        playerHealth.AddFruitValue(fruitValue);
+        scoreManager.AddScore(scoreValue);
+        gameObject.SetActive(false);
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.x < DestructionPoint.transform.position.x)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ AnimalSurvival/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Transform platformGenerator;
+    private Vector3 platformStartPoint;
+
+    public PlayerController thePlayer;
+    private Vector3 playerStartPoint;
+
+    public GroundGenerator groundGenerator;
+    private Vector3 groundGeneretorStartPosition;
+
+    // Checks for destroyed platforms.
+    private Destroyer[] destructionList;
+    private DestroyByContact[] destroyByContactList;
+
+    private ScoreManager scoreManager;
+    private DesertDeathMenu deathMenu;
+
+    // Use this for initialization
+    void Start () {
+        // TODO This we need.
+        platformStartPoint = platformGenerator.position;
+        playerStartPoint = thePlayer.transform.position;
+        groundGeneretorStartPosition = groundGenerator.transform.position;
+        scoreManager = FindObjectOfType<ScoreManager>();
+        deathMenu = FindObjectOfType<DesertDeathMenu>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RestartGame()
+    {
+        scoreManager.scoreIncrease = false;
+        thePlayer.gameObject.SetActive(false);
+
+        deathMenu.gameObject.SetActive(true);
+    }
+
+    public void Reset()
+    {
+        destructionList = FindObjectsOfType<Destroyer>();
+        for (int i = 0; i < destructionList.Length; i++)
+        {
+            destructionList[i].gameObject.SetActive(false);
+        }
+
+        destroyByContactList = FindObjectsOfType<DestroyByContact>();
+        foreach (DestroyByContact o in destroyByContactList)
+        {
+            o.gameObject.SetActive(false);
+        }
+
+
+        groundGenerator.restart();
+
+        thePlayer.transform.position = playerStartPoint;
+        platformGenerator.position = platformStartPoint;
+
+        thePlayer.gameObject.SetActive(true);
+        scoreManager.scoreCount = 0;
+        scoreManager.scoreIncrease = true;
+    }
+}
